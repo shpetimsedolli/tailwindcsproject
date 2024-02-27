8cf5ca84aba51e49c3852a7afad90546225ef741
shpetimsedolli <shpeti101@gmail.com>
2024-02-27 19:31:19 +0100

    responsive added to section of Contact

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -7,26 +7,26 @@ import google from "../assets/btn.png";
 const Contact = () => {
   return (
     <>
-      <div className="container flex my-[100px] items-center justify-between ">
+      <div className="container flex my-[100px] items-center justify-between lg:flex-col lg:gap-11">
         <div className="flex flex-col">
-          <h1 className="font-bold text-[36px]">
+          <h1 className="font-bold text-[36px] lg:text-center">
             See why the world’s best <br /> companies use Qubly to{" "}
           </h1>
-          <div className="flex items-center gap-3">
-            <img src={line} alt="" />
+          <div className="flex items-center justify-center gap-3">
+            <img src={line} alt="" className=" sm:w-[70px]" />
             <p>Trusted By</p>
-            <img src={line} alt="" />
+            <img src={line} alt="" className=" sm:w-[70px]" />
           </div>
-          <div className="flex gap-[100px] mb-[70px] mt-[50px] items-center">
+          <div className="flex gap-[100px] mb-[70px] mt-[50px] items-center lg:gap-0 lg:justify-between">
             <img src={accenture} alt="" />
             <img src={react} alt="" />
           </div>
-          <div className="flex gap-[100px] items-center">
+          <div className="flex gap-[100px] items-center lg:gap-0 lg:justify-between">
             <img src={airbnb} alt="" />
             <img src={sky} alt="" />
           </div>
         </div>
-        <div className="flex flex-col items-center gap-[16px] p-[20px] box w-[416px]">
+        <div className="flex flex-col items-center gap-[16px] p-[20px] box w-[416px] lg:items-center lg:justify-center sm:w-[345px]">
           <h3 className="font-bold text-[21px]">Sign Up</h3>
           <input
             className="border-[#8989A2] border-[1px] py-[15px] pl-4 outline-none w-full"
@@ -49,7 +49,8 @@ const Contact = () => {
           <p>Or</p>
           <img src={google} alt="" />
           <p className="">
-            Already have an account?<span className="">Login</span>
+            Already have an account?
+            <span className="underline text-[blue] ml-2">Login</span>
           </p>
         </div>
       </div>
